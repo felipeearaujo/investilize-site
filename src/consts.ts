82591fb7f2bc85b667cfe0c0216601ba15d34cbb
Felipe <felipeads59@gmail.com>
2025-07-07 11:46:24 -0300

    Atualização do titulo do site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Investilize';
+export const SITE_DESCRIPTION = 'Ferramentas financeiras, calculadoras e conteúdo para ajudar você a tomar decisões mais inteligentes com o seu dinheiro.';
