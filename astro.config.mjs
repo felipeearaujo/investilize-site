8d28c4fb174472840e28712bfa1875c8b3c1ac3d
Felipe <felipeads59@gmail.com>
2025-09-30 09:52:06 -0300

    SITEMAP

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://investilize.com.br',
   integrations: [mdx(), sitemap()],
   adapter: netlify(),
 });
